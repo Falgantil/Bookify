130e2f4e793d7e828f525d1ef5d2d90c95837d22
Rick Boysen <rick.boysen@live.nl>
2016-08-11 18:07:06 +0200

    Added Unit-Test project to the solution

---
+++ Bookify/Bookify.UnitTets/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bookify.UnitTets
+{
+    [TestClass]
+    public class BookController
+    {
+        [TestMethod]
+        public void GetBook()
+        {
+        }
+
+        [TestMethod]
+        public void GetBook(int id)
+        {
+        }
+
+        [TestMethod]
+        public void BuyBook()
+        {
+        }
+    }
+}
