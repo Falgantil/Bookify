f0b20b239b8c2106a47922d6a5341d8c1824ec8d
Bjarke Søgaard <Ekrajb123@gmail.com>
2016-08-15 11:39:01 +0200

    Started implementing JWT.

--- Bookify/Bookify.API/Controllers/AuthController.cs
+++ Bookify/Bookify.API/Controllers/AuthController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Http;
+using Bookify.Core;
 
 namespace Bookify.API.Controllers
 {
     public class AuthController : ApiController
     {
+        private IAuthenticationService _authService;
+
+        public AuthController(IAuthenticationService authService)
+        {
+            this._authService = authService;
+        }
 
         [HttpPost]
         public async Task<IHttpActionResult> Login()
         {
+
             return Ok();
         }
 
---
+++ Bookify/Bookify.Core/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bookify.Core
+{
+    public interface IAuthenticationService
+    {
+        Task<string> CreateAuthToken(string username, string password);
+    }
+}
