2ae1fbad92d571dd9fedd0c82d6ba62817a1ea37
Bjarke Søgaard <Ekrajb123@gmail.com>
2016-08-17 16:17:32 +0200

    Updated the BookApi to support the new way of delivering book responses.

--- Bookify/Bookify.App/Bookify.App.Sdk/Implementations/BookApi.cs
+++ Bookify/Bookify.App/Bookify.App.Sdk/Implementations/BookApi.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Bookify.App.Sdk.Interfaces;
 using Bookify.Models;
+using Bookify.Models.ViewModels;
 using Newtonsoft.Json;
 
 namespace Bookify.App.Sdk.Implementations
@@ -28,7 +29,8 @@ namespace Bookify.App.Sdk.Implementations
             
             var response = await this.ExecuteRequest(request);
             var json = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<IEnumerable<Book>>(json);
+            var deserializeObject = JsonConvert.DeserializeObject<BookSearch>(json);
+            return deserializeObject.Books;
         }
 
         public async Task<Book> Get(int id)
