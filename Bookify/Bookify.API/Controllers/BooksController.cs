2be6b0c48bc000c2b1990fc979689948be980775
Rick Boysen <rick.boysen@live.nl>
2016-08-17 14:38:03 +0200

    Solved DependecyInjection, And output stream to mem...stream

--- Bookify/Bookify.API/Controllers/BooksController.cs
+++ Bookify/Bookify.API/Controllers/BooksController.cs
@@ -24,7 +24,7 @@ namespace Bookify.API.Controllers
         private readonly IBookOrderRepository _bookOrderRepository;
         private readonly IBookContentRepository _bookContentRepository;
         private readonly IBookFeedbackRepository _bookFeedbackRepository;
-
+        private readonly IFileServerRepository _fileServerRepository;
         public BooksController(IBookRepository bookRepository, IBookHistoryRepository bookHistoryRepository, IPersonRepository personRepository, IBookOrderRepository bookOrderRepository, IBookContentRepository bookContentRepository, IBookFeedbackRepository bookFeedbackRepository, IFileServerRepository fileServerRepository)
         {
             _bookRepository = bookRepository;
@@ -33,6 +33,7 @@ namespace Bookify.API.Controllers
             _bookOrderRepository = bookOrderRepository;
             _bookContentRepository = bookContentRepository;
             _bookFeedbackRepository = bookFeedbackRepository;
+            _fileServerRepository = fileServerRepository;
         }
 
         [HttpGet]
@@ -160,7 +161,7 @@ namespace Bookify.API.Controllers
         [HttpGet]
         public async Task<HttpResponseMessage> Cover(int id, int? width = null, int? height = null)
         {
-            MemoryStream stream = fileServerRepository.GetCoverFile(id);
+            MemoryStream stream = _fileServerRepository.GetCoverFile(id);
             if (stream == null) return new HttpResponseMessage(HttpStatusCode.InternalServerError);
 
             using (var ms = new MemoryStream())
