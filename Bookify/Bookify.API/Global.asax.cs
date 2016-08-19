ad51cbe831b77efe5600c59b5788189b937a59a1
Andreas Hansen <andqwerty@gmail.com>
2016-08-19 13:16:55 +0200

    Allow authorization header on the API

--- Bookify/Bookify.API/Global.asax.cs
+++ Bookify/Bookify.API/Global.asax.cs
@@ -18,7 +18,7 @@ namespace Bookify.API
             {
                 HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
                 HttpContext.Current.Response.End();
             }
