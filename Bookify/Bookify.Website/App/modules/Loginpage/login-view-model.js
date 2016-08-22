caca2f40743255f953a42a83dce53bedfcd5cb98
Rick Boysen <rick.boysen@live.nl>
2016-08-22 14:46:48 +0200

    Add prop Alias to AutTokenDto changing login method on website now saving infomation in a cookie

--- Bookify/Bookify.Website/App/modules/Loginpage/login-view-model.js
+++ Bookify/Bookify.Website/App/modules/Loginpage/login-view-model.js
@@ -2,7 +2,7 @@ import bookifyapi from '../util/bookifyapi';
 import {observable, computed} from "mobx";
 
 class LoginViewModel {
-@observable email = '';
+@observable email = 'admin@test.com';
 @observable password = '';
 
   constructor() {
@@ -10,6 +10,9 @@ class LoginViewModel {
 
   async submit() {
     var result = await bookifyapi.login(this.email, this.password);
+    $.cookie = "AuthToken="+result.Token+";role="+result.Roles;
+    console.log("Token: " + result.Token);
+    console.log("Roles: " + result.Roles);
     return result;
   }
 }
--- Bookify/Bookify.Website/App/modules/util/bookifyapi.js
+++ Bookify/Bookify.Website/App/modules/util/bookifyapi.js
@@ -1,16 +1,17 @@
 import $ from 'jquery';
 import http from './http';
 
-//let baseUrl = 'http://bookifyapi.azurewebsites.net/';
-let baseUrl = 'http://localhost:13654/';
-let authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3NkYXRlIjoxNDcxNjA5MTMyLCJleHBkYXRlIjoxNTAzMTQ1MTMyLCJ1c2VyaWQiOjV9.fyv68ofK4E8lyE7hJTVVG9QgY85dWC4YbwkAF7CN4yY';
+let baseUrl = 'http://bookifyapi.azurewebsites.net/';
+//let baseUrl = 'http://localhost:13654/';
+//let authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3NkYXRlIjoxNDcxNjA5MTMyLCJleHBkYXRlIjoxNTAzMTQ1MTMyLCJ1c2VyaWQiOjV9.fyv68ofK4E8lyE7hJTVVG9QgY85dWC4YbwkAF7CN4yY';
 
 class BookifyAPI {
   getBaseUrl() { return baseUrl; }
-  getAuthToken() { return authToken; }
+  //getAuthToken() { return authToken; }
+  //getRoles() { return roles; }
 
   // Books
-  getBooks() { return http.get(baseUrl + 'books');  }
+  getBooks() { return http.get(baseUrl + 'books'); }
   getBook(id) { return http.get(baseUrl + 'books/' + id); }
   getBookFeedback(id) { return http.get(baseUrl + 'books/getbookfeedback/' + id); }
   getBookThumbnailSrc(id) { return baseUrl + 'files/' + id + '/downloadcover'; }
@@ -19,14 +20,14 @@ class BookifyAPI {
   postBook(book) { http.post(baseUrl + 'books', book, { headers: { 'Authorization': 'jwt ' + authToken } }); }
   postBookCover(bookId, data, callback) {
     $.ajax({
-      url: 'http://localhost:13654/' + 'files/' + bookId + '/UploadCover',
+      url: baseUrl + 'files/' + bookId + '/UploadCover',
       type: 'POST',
       processData: false,
       contentType: false,
       header: { 'Authorization': 'jwt ' + authToken },
       data: data,
       success: function () { callback(true); },
-      error: function() { callback(false); }
+      error: function () { callback(false); }
     });
   }
 
@@ -35,13 +36,15 @@ class BookifyAPI {
   // Auth
   async login(email, password) {
     var result = await http.post(baseUrl + 'auth/login', { email, password });
-    if (result.Token.length > 0) {
-      authToken = result.Token;
-      return true;
-    }
-    return false;
+    // if (result.Token.length > 0) {
+    //   authToken = result.Token;
+    //   return result;
+    // }
+    // return false;
+    return result
   }
   logout() { authToken = ''; }
+
 
 }
 
