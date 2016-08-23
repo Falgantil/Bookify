a336f0b0a884ed84d4c634ab85dad1a2c9279bc8
Jonas Thorsen <162534@live.dk>
2016-08-23 12:37:59 +0200

    Added language to BookDto Fixed book update and feedback update

--- Bookify/Bookify.Common/Models/BookDto.cs
+++ Bookify/Bookify.Common/Models/BookDto.cs
@@ -11,6 +11,7 @@ namespace Bookify.Common.Models
         public string ISBN { get; set; }
 
         public decimal Price { get; set; }
+        public string Language { get; set; }
 
         public int PublishYear { get; set; }
         public double AverageRating { get; set; }
