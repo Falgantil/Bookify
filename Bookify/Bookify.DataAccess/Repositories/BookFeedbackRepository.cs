b2806c0d2a892a8505fcf931e6580d1421bf00ae
Andreas Hansen <andqwerty@gmail.com>
2016-08-23 09:30:47 +0200

    Changed the DeleteFeedback method to support the clustered key

--- Bookify/Bookify.DataAccess/Repositories/BookFeedbackRepository.cs
+++ Bookify/Bookify.DataAccess/Repositories/BookFeedbackRepository.cs
@@ -62,9 +62,10 @@ namespace Bookify.DataAccess.Repositories
             return updatedFeedback.ToDto();
         }
 
-        public async Task DeleteFeedback(int id)
+        public async Task DeleteFeedback(int bookId, int personId)
         {
-            var feedback = await this.Find(id);
+            var feedback = await this.Context.BookFeedback.Where(x => x.BookId == bookId && x.PersonId == personId).SingleAsync();
+            //var fb2 = this.Context.BookFeedback.Find(new { BookId = bookId, PersonId = personId });
             await this.Remove(feedback);
 
         }
