9c7c4bfd8a47cddffbb1f27c4c93bbf821fc4720
Andreas Hansen <andqwerty@gmail.com>
2016-08-23 08:40:43 +0200

    Add math.round to rating view. to fix ratings with decimals

--- Bookify/Bookify.Website/App/modules/Shared/rating-view.js
+++ Bookify/Bookify.Website/App/modules/Shared/rating-view.js
@@ -5,8 +5,8 @@ class RatingView extends React.Component {
   render(){
     return (
       <div className="icon-ratings hearts">
-      {_.range(0, this.props.value).map((index) => (<div className="icon-rating-full" key={index}></div>))}
-      {_.range(this.props.value, 5).map((index) => (<div className="icon-rating-empty" key={index}></div>))}
+      {_.range(0, Math.round(this.props.value)).map((index) => (<div className="icon-rating-full" key={index}></div>))}
+      {_.range(Math.round(this.props.value), 5).map((index) => (<div className="icon-rating-empty" key={index}></div>))}
       </div>
     );
   }
