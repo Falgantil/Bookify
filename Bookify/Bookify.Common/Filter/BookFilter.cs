32d48a1888f4e9d9a1326e88b49f89f11b6c9dd8
Bjarke Søgaard <Ekrajb123@gmail.com>
2016-08-24 10:32:59 +0200

    Removed a few unnecessary properties on the book filter.

--- Bookify/Bookify.Common/Filter/BookFilter.cs
+++ Bookify/Bookify.Common/Filter/BookFilter.cs
@@ -2,8 +2,6 @@ namespace Bookify.Common.Filter
 {
     public class BookFilter : BaseFilter
     {
-        public bool MyBooks { get; set; }
-
         public int[] Genres { get; set; }
 
         public int? Author { get; set; }
@@ -11,6 +9,5 @@ namespace Bookify.Common.Filter
         public string OrderBy { get; set; } = "Price";
 
         public bool Descending { get; set; } = false;
-        public int? PersonId { get; set; }
     }
 }
