867e16d85cf2cbab9d49eb9a3dc847f827a6557e
Jonas Thorsen <162534@live.dk>
2016-08-25 08:13:09 +0200

    A person cannot borrow or queue the same book twice unless the previous BookOrder expires

--- Bookify/Bookify.DataAccess/Repositories/BookOrderRepository.cs
+++ Bookify/Bookify.DataAccess/Repositories/BookOrderRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Bookify.Common.Commands.Auth;
 using Bookify.Common.Enums;
+using Bookify.Common.Exceptions;
 using Bookify.Common.Repositories;
 using Bookify.DataAccess.Models;
 
@@ -16,22 +17,27 @@ namespace Bookify.DataAccess.Repositories
 
         }
 
+        
         public async Task CreateOrder(CreateOrderCommand command)
         {
+            string msg = $"The book with the id: {command.BookId} has already been borrowed or queued by the personId: {command.PersonId}";
             if (command.Status == BookOrderStatus.Borrowed)
             {
-                // Get all orders where status is borrowed & bookId is book we are searching for & the date is valid (returndate has to be more than current date)
-                var orders = Context.BookOrders.Where(x => x.Status == BookOrderStatus.Borrowed && x.BookId == command.BookId && x.ReturnDatetime > DateTime.Now);
+                // Get all orders where bookId is the book we are searching for & the date is valid (returndate has to be more than current date)
+                var orders = Context.BookOrders.Where(x => x.BookId == command.BookId && x.ReturnDatetime > DateTime.Now || x.ReturnDatetime == null);
+                // Check if person already has a valid BookOrder 
+                if (orders.Where(x => x.Status == BookOrderStatus.Borrowed || x.Status == BookOrderStatus.Queued).Any(x => x.PersonId == command.PersonId))
+                    throw new BadRequestException(msg);
                 var book = await Context.Books.Where(x => x.Id == command.BookId).SingleAsync();
                 // if amount of copies is less or equal to amount of orders, borrow it, else queue it
-                if (book.CopiesAvailable > orders.Count())
+                if (book.CopiesAvailable > orders.Count(x => x.Status == BookOrderStatus.Borrowed))
                 {
                     await this.Add(new BookOrder
                     {
                         BookId = command.BookId,
                         Created = DateTime.Now,
                         PersonId = command.PersonId,
-                        ReturnDatetime = DateTime.Now.AddMonths(1),
+                        ReturnDatetime = DateTime.Now.AddDays(30),
                         Status = BookOrderStatus.Borrowed
                     });
                 }
